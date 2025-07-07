9386f5f809c02e35043df14ad7b6c93a92e45392
enyichiaagu <enyichiaagu@gmail.com>
2025-07-07 10:12:15 +0100

    feat: Added server documentation

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 import pingRouter from "./routes/ping.router.js";
 import translationsRouter from "./routes/translations.router.js";
 import articlesRouter from "./routes/articles.router.js";
+import docsRouter from "./routes/docs.router.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +14,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/", docsRouter);
 app.use("/ping", pingRouter);
 app.use("/translations", translationsRouter);
 app.use("/docs", articlesRouter);
